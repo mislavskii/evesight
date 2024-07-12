1049e4a2e304799303ade7b7db6c80f2f4eb7cd2
mislavskii <jornjat@gmail.com>
2024-07-12 17:24:41 +0700

    added data field to Plot model

--- analyzer/models.py
+++ analyzer/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Plot(models.Model):
     session_id = models.CharField(max_length=128, primary_key=True)
+    data = models.TextField(default='')
     mean_delivered = models.BinaryField()
     top_delivered = models.BinaryField()
     mean_received = models.BinaryField()
